cbbedf0c75fc2e84fba69e81c7a5ada89ad7a30a
Dhederosade <febriansyahrevan7@gmail.com>
2020-03-21 04:47:18 +0700

    initial commit

---
+++ Aplikasi.java
@@ -0,0 +1,7 @@
+public class Aplikasi{
+    public static void main(String[] args) {
+        Mahasiswa mhs = new Mahasiswa("18090091", "Revan Febriansyah", "4A");
+        System.out.println(mhs.info());
+        
+    }
+}
